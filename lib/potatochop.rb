b0158d447699f2eeee2080329bc9ef67ca89bdd9
John Mertens <john.mertens@gmail.com>
2014-03-29 17:02:06 -0700

    Add the first draft of the haml processor

--- lib/potatochop.rb
+++ lib/potatochop.rb
@@ -1,10 +1,16 @@
 require 'potatochop/version'
 require 'sinatra/base'
+require 'haml'
 
 module Potatochop
   class Web < Sinatra::Base
-    get '/' do
-      "Hello worldz."
+    get '/*.html' do
+      file_path = File.join(settings.working_dir, "#{params[:splat][0]}.html.haml")
+      if File.exists? file_path
+        Haml::Engine.new(File.read(file_path)).render
+      else
+        404
+      end
     end
   end
 end
