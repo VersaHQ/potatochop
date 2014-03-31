29876814adf5bf213f5b3ec850ef30d2c237b3fa
John Mertens <john.mertens@gmail.com>
2014-03-30 21:54:17 -0700

    Whitespace

--- lib/potatochop.rb
+++ lib/potatochop.rb
@@ -6,25 +6,24 @@ require 'sass'
 
 module Potatochop
   class Web < Sinatra::Base
-
     get '/*.html' do
       tater = Potatochop::Spud.new(settings.working_dir)
       return_str = tater.get_html(params[:splat][0])
       return_str.nil? ? 404 : return_str
     end
-    
+
     get '/*.css' do
       tater = Potatochop::Spud.new(settings.working_dir)
       return_str = tater.get_css(params[:splat][0])
       content_type 'text/css', :charset => 'utf-8'
       return_str.nil? ? 404 : return_str
     end
-    
+
     get %r{/(.*).(png|jpg|jpeg|gif)} do
       file_path = File.join(settings.working_dir, "#{params[:captures][0]}.#{params[:captures][1]}")
       File.exists? file_path ? send_file(file_path) : 404
     end
-    
+
     get '/*.js' do
       file_path = File.join(settings.working_dir, "#{params[:splat][0]}.js")
       File.exists? file_path ? send_file(file_path) : 404
