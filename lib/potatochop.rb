618621ec5297f872e6b295d32e491f7951fbe5a9
John Mertens <john.mertens@gmail.com>
2014-04-15 16:37:36 -0700

    Move the sinatra stuff into it's own lib file.

--- lib/potatochop.rb
+++ lib/potatochop.rb
@@ -1,32 +1,15 @@
 require 'potatochop/version'
 require 'potatochop/spud'
-require 'sinatra/base'
+require 'potatochop/file_system_interface'
+require 'potatochop/configuration'
+require 'potatochop/web'
 require 'haml'
 require 'sass'
 
 module Potatochop
-  class Web < Sinatra::Base
-    get '/*.html' do
-      tater = Potatochop::Spud.new(settings.working_dir)
-      return_str = tater.get_html(params[:splat][0])
-      return_str.nil? ? 404 : return_str
-    end
-
-    get '/*.css' do
-      tater = Potatochop::Spud.new(settings.working_dir)
-      return_str = tater.get_css(params[:splat][0])
-      content_type 'text/css', :charset => 'utf-8'
-      return_str.nil? ? 404 : return_str
-    end
-
-    get %r{/(.*).(png|jpg|jpeg|gif)} do
-      file_path = File.join(settings.working_dir, "#{params[:captures][0]}.#{params[:captures][1]}")
-      File.exists? file_path ? send_file(file_path) : 404
-    end
-
-    get '/*.js' do
-      file_path = File.join(settings.working_dir, "#{params[:splat][0]}.js")
-      File.exists? file_path ? send_file(file_path) : 404
-    end
+  def self.start_up(args)
+    config = Configuration.process(args)
+    # Potatochop::Spud.new(config[:interface])
+    Potatochop::Web.run!(:tater => Potatochop::Spud.new(config[:interface]))
   end
 end
