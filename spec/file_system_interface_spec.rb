2bb71c6348cf014a473139766a51c2811bad61b2
John Mertens <john.mertens@gmail.com>
2014-09-11 16:15:01 -0700

    Change some tests to the expect syntax to make them pass on Travis.

--- spec/file_system_interface_spec.rb
+++ spec/file_system_interface_spec.rb
@@ -10,21 +10,21 @@ describe Potatochop::FileSystemInterface do
   describe '#exists?' do
     it 'returns true if the given file exists' do
       fs = Potatochop::FileSystemInterface.new('spec/fake_mockups')
-      fs.exists?('foo.html.haml').should be_true
+      expect(fs.exists?('foo.html.haml')).to be_true
     end
-    
+
     it 'returns false if the given file does not exist' do
       fs = Potatochop::FileSystemInterface.new('spec/fake_mockups')
-      fs.exists?('not_real.html').should be_false
+      expect(fs.exists?('not_real.html')).to be_false
     end
   end
-  
+
   describe '#read' do
     it 'returns the content of the given files' do
       fs = Potatochop::FileSystemInterface.new('spec/fake_mockups')
       fs.read('foo.html.haml').should == File.read('spec/fake_mockups/foo.html.haml')
     end
-    
+
     it 'returns false if the given file does not exist' do
       fs = Potatochop::FileSystemInterface.new('spec/fake_mockups')
       expect { fs.read('not_real.html') }.to raise_error
