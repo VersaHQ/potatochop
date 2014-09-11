60d661ef2d2bd46b8b527341662f778e711430eb
John Mertens <john.mertens@gmail.com>
2014-09-11 11:18:40 -0700

    Make the source of the interface also the base load path for Sass engine (a.k.a. 'make mixins work').

--- lib/potatochop/spud.rb
+++ lib/potatochop/spud.rb
@@ -3,11 +3,11 @@ module Potatochop
     def initialize(interface)
       @interface = interface
     end
-    
+
     def get_file(file_name)
       @interface.exists?(file_name) ? @interface.read(file_name) : nil
     end
-    
+
     def get_html(file_name)
       if @interface.exists?("#{file_name}.html") # Static html first
         @interface.read("#{file_name}.html")
@@ -15,12 +15,12 @@ module Potatochop
         Haml::Engine.new(@interface.read("#{file_name}.html.haml")).render
       end
     end
-    
+
     def get_css(file_name)
       if @interface.exists?("#{file_name}.css")  # Static css
         @interface.read("#{file_name}.css")
       elsif @interface.exists?("#{file_name}.css.scss") # Sass css
-        Sass::Engine.new(@interface.read("#{file_name}.css.scss"), :syntax => :scss).render
+        Sass::Engine.new(@interface.read("#{file_name}.css.scss"), :syntax => :scss, :load_paths => [@interface.source]).render
       end
     end
   end
